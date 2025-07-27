eea77fa2075bb20014096b5ac07b9fed34a33247
Boobies <dmychel@tutanota.com>
2025-07-27 12:29:32 -0400

    removed router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Router, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 

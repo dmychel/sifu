c0b5efe93f8805dc53887fcff41bc4302afedbfa
Boobies <dmychel@tutanota.com>
2025-07-29 22:05:32 -0400

    rendered arenas component

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,7 +1,12 @@
+import Arenas from "../components/Arenas"
 import Hero from "../components/Hero"
 
 export default function Index() {
     return (
-        <Hero />
+        <>
+            <Hero />
+            <Arenas />
+        </>
+
     )
 }
